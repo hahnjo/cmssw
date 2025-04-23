6837888dd665482a3ba3d0b8a98cd8274042362d
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-04-23 14:42:53 +0200

    MPI_Barrier preBeginJob and endProcessing

--- HeterogeneousCore/MPIServices/interface/MPIService.h
+++ HeterogeneousCore/MPIServices/interface/MPIService.h
@@ -2,11 +2,15 @@
 #define HeterogeneousCore_MPIServices_interface_MPIService_h
 
 #include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
+#include "FWCore/ServiceRegistry/interface/ActivityRegistry.h"
 
 class MPIService {
 public:
-  MPIService(edm::ParameterSet const& config);
+  MPIService(edm::ParameterSet const& config, edm::ActivityRegistry& registry);
   ~MPIService();
+
+  void preBeginJob(edm::ProcessContext const&);
+  void endProcessing();
 
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
   static void required();
