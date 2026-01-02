fd26acef566c78cba0380544964c872c69fd0f8f
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2026-01-02 14:22:26 +0100

    RNTupleTempOutputModuleMPI with RNTupleWriterMPI

--- FWIO/RNTupleTempOutput/plugins/Module.cc
+++ FWIO/RNTupleTempOutput/plugins/Module.cc
@@ -1,8 +1,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWIO/RNTupleTempOutput/interface/RNTupleTempOutputModule.h"
+#include "FWIO/RNTupleTempOutput/interface/RNTupleTempOutputModuleMPI.h"
 #include "FWIO/RNTupleTempOutput/interface/TimeoutRNTupleTempOutputModule.h"
 
 using edm::rntuple_temp::RNTupleTempOutputModule;
+using edm::rntuple_temp::RNTupleTempOutputModuleMPI;
 using edm::rntuple_temp::TimeoutRNTupleTempOutputModule;
 DEFINE_FWK_MODULE(RNTupleTempOutputModule);
+DEFINE_FWK_MODULE(RNTupleTempOutputModuleMPI);
 DEFINE_FWK_MODULE(TimeoutRNTupleTempOutputModule);
